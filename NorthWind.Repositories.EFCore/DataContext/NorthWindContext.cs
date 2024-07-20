969dd5a350308f4a0760ca8784c9f8682d0d4f17
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-19 20:14:02 -0700

    init: EFCore y migrations agregados

---
+++ NorthWind.Repositories.EFCore/DataContext/NorthWindContext.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWind.Entities.POCOEntitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.DataContext
+{
+    public class NorthWindContext : DbContext
+    {
+        public NorthWindContext(DbContextOptions<NorthWindContext> options) :
+            base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region Customer
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.ID)
+                .HasMaxLength(5)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(40);
+            #endregion
+            #region Product
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(40);
+            #endregion
+            #region Order
+            modelBuilder.Entity<Order>()
+                .Property(o => o.CustomerID)
+                .IsRequired()
+                .HasMaxLength(5)
+                .IsFixedLength(); //longitud fija
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipAddress)
+                .IsRequired()
+                .HasMaxLength(60);
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipCity)
+                .HasMaxLength(15);
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipCountry)
+                .HasMaxLength (15);
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipPostalCode)
+                .HasMaxLength(10);
+            #endregion
+            #region Primary Key
+            modelBuilder.Entity<OrderDetail>()
+                .HasKey(od => new { od.OrderId, od.ProductId }); //De esta manera esta compuesta el primary de key de OrderDetail
+
+            modelBuilder.Entity<Order>()
+                .HasOne<Customer>()
+                .WithMany()
+                .HasForeignKey(o => o.CustomerID); //Relacion entre ordenes y customer
+
+            modelBuilder.Entity<OrderDetail>()
+                .HasOne<Product>()
+                .WithMany()
+                .HasForeignKey(od => od.ProductId);
+            #endregion
+            #region Prueba
+            modelBuilder.Entity<Product>()
+                .HasData(
+                new Product {ID = 1, Name = "Chai"},
+                new Product { ID = 2, Name = "Chang" },
+                new Product { ID = 3, Name = "Anissed Syrup" }
+                );
+
+            modelBuilder.Entity<Customer>()
+                .HasData(
+                new Customer { ID = "ALFKI", Name = "Alfreds F."},
+                new Customer { ID = "ANATR", Name = "Ana Trujillo" },
+                new Customer { ID = "ANTON", Name = "Antonio Moreno" }
+                );
+            #endregion
+
+        }
+    }
+}
---
+++ NorthWind.Repositories.EFCore/Migration/20240720025612_InitialCreate.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NorthWind.Repositories.EFCore.Migration
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "nchar(5)", fixedLength: true, maxLength: 5, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerID = table.Column<string>(type: "nchar(5)", fixedLength: true, maxLength: 5, nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ShipAddress = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    ShipCity = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ShipCountry = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ShipPostalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    DiscountType = table.Column<int>(type: "int", nullable: false),
+                    Discount = table.Column<double>(type: "float", nullable: false),
+                    ShippingType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customers",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<short>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => new { x.OrderId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { "ALFKI", "Alfreds F." },
+                    { "ANATR", "Ana Trujillo" },
+                    { "ANTON", "Antonio Moreno" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Chai" },
+                    { 2, "Chang" },
+                    { 3, "Anissed Syrup" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerID",
+                table: "Orders",
+                column: "CustomerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
