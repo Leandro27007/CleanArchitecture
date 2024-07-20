1fe2ac054f4adca0449402cf68d648822fd955ef
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-19 20:11:47 -0700

    chore: tipo de dato cambiado

--- NorthWind.Entities/POCOEntitites/Customer.cs
+++ NorthWind.Entities/POCOEntitites/Customer.cs
@@ -8,7 +8,7 @@ namespace NorthWind.Entities.POCOEntitites
 {
     public class Customer
     {
-        public int ID { get; set; }
+        public string ID { get; set; }
 
         public string Name { get; set; }
     }
